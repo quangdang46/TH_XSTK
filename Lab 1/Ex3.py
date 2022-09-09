da77225a9418f6fa653468f988d39ea9c122ef31
quangdang46 <tranquangdang21@gmail.com>
2022-09-10 01:38:58 +0700

    lab 1

--- Lab 1/Ex3.py
+++ Lab 1/Ex3.py
@@ -1,3 +1,10 @@
 import itertools
-# a
-U3 = list(itertools.combinations(['M', 'P', 'C', 'L'], 10))
+U3=list(itertools.permutations([['M']*4, ['P']*3, ['C']*2, ['L']*1], 4))
+for i in range(len(U3)):
+  U3[i]=list(U3[i])
+  U3[i]=list(itertools.chain(*U3[i]))
+print(U3)
+print(U3.__len__())
+
+
+
