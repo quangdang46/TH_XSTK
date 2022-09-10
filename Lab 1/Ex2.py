5218e18722719c679e13b766f5e31b77be6fb7b8
quangdang46 <tranquangdang21@gmail.com>
2022-09-10 09:27:43 +0700

    new

--- Lab 1/Ex2.py
+++ Lab 1/Ex2.py
@@ -1,16 +1,25 @@
 import itertools
+list_ball=['w'+str(i) for i in range(8)]+['B'+str(i) for i in range(6)]+['R'+str(i) for i in range(9)]
 #a
-U3=list(itertools.combinations_with_replacement(['W', 'B', 'R'], 3))
+
+U3=list(itertools.combinations(list_ball, 3))
 print(U3)
 print(len(U3))
-#b
-U3=list(itertools.combinations(['W', 'B', 'R'], 3))
+# #b
+list_ball_2=[['w'+str(i) for i in range(8)], ['B'+str(i) for i in range(6)], ['R'+str(i) for i in range(9)]]
+U3=list(itertools.product(*list_ball_2))
 print(U3)
 print(len(U3))
-#c
-U3=list(itertools.combinations(['W', 'R'], 2))
-for i in range(len(U3)):
-  U3[i]=list(U3[i])
-  U3[i].append('B')
-print(U3)
-print(len(U3))
+# #c
+list_ball_3=[['w'+str(i) for i in range(8)], ['R'+str(i) for i in range(6)]]
+list_ball_3_b=[['B'+str(i) for i in range(9)]]
+
+
+
+
+U3_wr=list(itertools.product(*list_ball_3))
+U3_b=list(itertools.product(*list_ball_3_b))
+U3_wr_b=list(itertools.product(U3_wr, U3_b))
+
+
+
--- Lab 1/Ex3.py
+++ Lab 1/Ex3.py
@@ -1,7 +1,21 @@
 import itertools
-U3=list(itertools.permutations([['M']*4, ['P']*3, ['C']*2, ['L']*1], 4))
-for i in range(len(U3)):
-  U3[i]=list(U3[i])
-  U3[i]=list(itertools.chain(*U3[i]))
-print(U3)
-print(U3.__len__())
+list_M=['M'+str(i) for i in range(4)]
+list_P=['P'+str(i) for i in range(3)]
+list_C =['C'+str(i) for i in range(2)]
+list_L=['L'+str(i) for i in range(1)]
+U3_M=list(itertools.permutations(list_M))
+U3_P=list(itertools.permutations(list_P))
+U3_C=list(itertools.permutations(list_C))
+U3_L=list(itertools.permutations(list_L))
+U3_MPC=list(itertools.product(U3_M, U3_P, U3_C, U3_L))
+print(U3_M)
+
+
+
+
+
+# for i in range(len(U3)):
+#   U3[i]=list(U3[i])
+#   U3[i]=list(itertools.chain(*U3[i]))
+# print(U3)
+
