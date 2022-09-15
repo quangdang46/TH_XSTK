dc6d78f6164b6c9055f432f6d473070487f7dedd
quangdang46 <tranquangdang21@gmail.com>
2022-09-15 17:05:53 +0700

    2c

--- Lab 1/Ex2.py
+++ Lab 1/Ex2.py
@@ -2,27 +2,19 @@ import itertools
 list_ball=['w'+str(i) for i in range(8)]+['B'+str(i) for i in range(6)]+['R'+str(i) for i in range(9)]
 #a
 
-U3=list(itertools.combinations(list_ball, 3))
-print(U3)
-print(len(U3))
+U2=list(itertools.combinations(list_ball, 3))
+print(U2)
+print(len(U2))
 # #b
 list_ball_2=[['w'+str(i) for i in range(8)], ['B'+str(i) for i in range(6)], ['R'+str(i) for i in range(9)]]
-U3=list(itertools.product(*list_ball_2))
-print(U3)
-print(len(U3))
+U2=list(itertools.product(*list_ball_2))
+print(U2)
+print(len(U2))
 # #c
 
-def cross_join(A,B):
-  return {(a,b) for a in A for b in B }
-list_ball_3=[['w'+str(i) for i in range(8)], ['R'+str(i) for i in range(6)]]
-list_ball_3_b=[['B'+str(i) for i in range(9)]]
-
-U3_wr=list(itertools.product(*list_ball_3))
-U3_b=list(itertools.product(*list_ball_3_b))
-U3_wr_b=cross_join(U3_wr,U3_b)
-print(U3_wr_b)
-print(len(U3_wr_b))
-
-
-
-
+U2_wrb=list(itertools.permutations(list_ball, 3))
+def check(arr):
+  return arr[0][0] =='w' and arr[1][0]=='R'
+result=list(filter(check, U2_wrb))
+print(len(result))
+print(result)
