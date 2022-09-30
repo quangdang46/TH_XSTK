aa2a893e6f5eeb64c83af75418e1a627083d15b6
quangdang46 <tranquangdang21@gmail.com>
2022-09-30 08:41:45 +0700

    ex3

--- Lab 2/Ex3.py
+++ Lab 2/Ex3.py
@@ -1,6 +1,6 @@
 from fractions import Fraction
 from random import *
-def simualtor_dice1(n):
+def simualtor_dice3(n):
   count = 0
   for i in range(n):
     die_1=randint(1,6)
@@ -9,4 +9,4 @@ def simualtor_dice1(n):
       count+=1
   return Fraction(count, n)
 
-print(simualtor_dice1(1000000))
+print(simualtor_dice3(1000000))
