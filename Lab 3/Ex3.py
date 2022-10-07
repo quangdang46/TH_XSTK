f3be7cf708a5ad383d04e73b6620ff4a13745f76
quangdang46 <tranquangdang21@gmail.com>
2022-10-07 22:26:22 +0700

    ex3

---
+++ Lab 3/Ex3.py
@@ -0,0 +1,34 @@
+from itertools import *
+from random import *
+def P(event, space):
+  return len(event)/len(space)
+
+Ranks = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'}
+Suits = {'♡', '♢', '♣', '♠'}
+# '♡', '♢': red
+# '♣', '♠': black
+
+# a
+Cards =set(map(lambda x:str(x[0])+x[1], product(Ranks, Suits)))
+# print(Cards)
+
+# b
+
+B = list(permutations(Cards, 3))
+# print(B)
+
+# c
+A1 = set(filter(lambda x: x.count('K') in {1, 2},list(map(lambda x: ''.join(x), list(permutations(Cards, 3))))))
+# print(A1)
+
+# d
+A2 = set(filter(lambda x: x.count('K')>=1,list(map(lambda x: ''.join(x), list(permutations(Cards, 3))))))
+print(P(A1, B))
+print(P(A2, B))
+
+
+
+
+
+
+
