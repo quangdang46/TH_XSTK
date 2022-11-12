29057ad958014e890575a8b2516474d240410776
quangdang46 <tranquangdang21@gmail.com>
2022-11-12 11:47:15 +0700

    ex3

---
+++ Lab 9/Exercise 3/main.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+with open('data.txt', 'r') as f:
+  text = f.read()
+
+words = text.split()
+freq = {}
+for word in words:
+  if word in freq:
+    freq[word] += 1
+  else:
+    freq[word] = 1
+
+
+sorted_freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+top_30 = sorted_freq[:30]
+
+words, frequencies = zip(*top_30)
+
+frequencies = np.array(frequencies)
+
+words = np.array(words)
+
+
+fig = plt.figure(figsize=(10, 10))
+plt.bar(words, frequencies)
+plt.title('Frequency of the 30 most common words')
+plt.xlabel('Words')
+plt.ylabel('Frequency')
+plt.xticks(rotation=90)
+plt.show()
+
+
+
+
+
