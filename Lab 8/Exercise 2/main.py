51768f71dae72c77cd9788f6553d7674725108e4
quangdang46 <tranquangdang21@gmail.com>
2022-11-20 13:45:02 +0700

    ex2

--- Lab 8/Exercise 2/main.py
+++ Lab 8/Exercise 2/main.py
@@ -2,13 +2,33 @@ import pandas as pd
 import numpy as np
 
 
+data=pd.read_csv('population.csv',delimiter=',')
+print(data.head(5))
 
+def count(data):
+  return len(data)
 
+def mean(data):
+  return np.mean(data)
 
+def std(data):
+  return np.sum((data - np.mean(data))**2) / len(data)
 
+def min(data):
+  return np.min(data)
 
+def max(data):
+  return np.max(data)
 
+# 'Country Name', 'Country Code','Mean','Std','Min','Max'
 
+df=pd.DataFrame(data,columns=['Country Name', 'Country Code','Mean','Std','Min','Max'])
+df['Mean']=data.groupby(['Country Name','Country Code'])['Year'].transform(mean)
+df['Std']=data.groupby(['Country Name','Country Code'])['Year'].transform(std)
+df['Min']=data.groupby(['Country Name','Country Code'])['Year'].transform(min)
+df['Max']=data.groupby(['Country Name','Country Code'])['Year'].transform(max)
+df=df.drop_duplicates()
 
+print(df)
 
 
